4ca021532b516e08faab619c36955796084a5e1a
Manu Thamer <yanceen@yahoo.com>
2020-11-01 22:26:34 +0100

    bribbing

--- hackertutorial.js
+++ hackertutorial.js
@@ -31,3 +31,93 @@ function rotLeft(a, d) {
     return a;
 
 }
+
+
+//EXERCISE 2 
+function minimumBribes(q) {
+
+    console.log("--minimum Bribes --");
+
+    var countBribes = 0;
+    var swappedNumbers = [];
+
+    //getting start array
+    //not necessary - sorting array ascending! 
+    // var start = [];
+    // for (let i = 0; i < q.length; i++) {
+    //     start.push(i + 1);
+    // }
+    // console.log("startArray: " + start);
+
+    console.log(q);
+
+
+    function swap(q) {
+
+        for (let i = 0; i < q.length; i++) {
+            if (q[i] > q[i + 1]) {
+                swappedNumbers.push(q[i]);
+                //swappedNumbers.push(q[i + 1]);
+                let x = q[i];
+                q[i] = q[i + 1];
+                q[i + 1] = x;
+                countBribes++;
+                console.log(swappedNumbers);
+            }
+            console.log("after swap: " + q + " countBribes: " + countBribes);
+        }
+
+
+        for (let i = 0; i < q.length; i++) {
+            if (q[i] > q[i + 1]) {
+                swap(q);
+            }
+        }
+
+
+    }
+
+
+    swap(q);
+
+
+
+    console.log("swappedNumbers: " + swappedNumbers.sort());
+
+
+    
+
+    var numberCounts = swappedNumbers.reduce(function(obj, number) {
+        console.log(obj, number);
+        if (!obj[number]) {
+            obj[number] = 1;
+        } else {
+            obj[number]++;
+        }
+        return obj;
+    }, {});
+
+
+    //  console.log("numberCounts: " + numberCounts);
+
+    var data = Object.values(numberCounts);
+
+    console.log("MAX von " + data);
+    var max = data.reduce(function(a, b) {
+        return Math.max(a, b);
+    });
+
+    console.log(max);
+
+
+        if (max > 2) {
+        output2.innerHTML = "Too chaotic";
+        console.log("Too chaotic");
+
+    } else {
+        output2.innerHTML = countBribes;
+        console.log(countBribes);
+    }
+
+
+}
